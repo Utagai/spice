df0963ffecc5db78519b0544e127d5ce3b69c50a
may <mmhoque@cs.stonybrook.edu>
2016-07-15 18:39:55 -0400

    Remove setup2.py

--- setup.py
+++ setup.py
@@ -9,6 +9,25 @@ setup(
     author = 'Mehrab Hoque',
     author_email = 'mehrabhoque@gmail.com',
     url = 'https://github.com/Utagai/spice', # use the URL to the github repo
+    classifiers=[
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
     keywords = ['spice', 'MyAnimeList', 'API'], # arbitrary keywords
     install_requires = ['requests', 'lxml', 'beautifulsoup4'],
 )
