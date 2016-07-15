abb88dfae1f351e53a4dfdfc07c585343b5a78ab
may <mmhoque@cs.stonybrook.edu>
2016-07-15 18:08:52 -0400

    Remove test.py from .gitignore.

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['requests', 'beautifulsoup4'],
+    install_requires=['requests', 'beautifulsoup4', 'lxml'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
