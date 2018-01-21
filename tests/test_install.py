571d0fc023389bdbdc6b73940abf0a437311cc93
may <mehrabhoque@gmail.com>
2018-01-21 18:32:45 -0500

    Fix syntax issue in install test. Remove py26 from travis.yml.

--- tests/test_install.py
+++ tests/test_install.py
@@ -1,4 +1,4 @@
 import spice_api as spice
 
 def test_install():
-    assert spice.user_agent = 'spice API (https://github.com/Utagai/spice)'
+    assert spice.user_agent == 'spice API (https://github.com/Utagai/spice)'
